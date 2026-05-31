1e7ba255cf4af62ed98492cd9662635b17597f19
Dennis <dennisbollettino@gmail.com>
2026-05-30 20:17:36 -0400

    updating js/config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_an1Y5IMHs',
+        userPoolClientId: '1jb1e6lr7stal8b7rv3j840bh4',
+        region: 'us-west-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
